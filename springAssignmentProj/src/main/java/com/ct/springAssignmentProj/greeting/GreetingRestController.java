9772b74cb3d652ccf0de482dbcc7bdb4c7ff9e63
Mesaric <niko.mesar@gmail.com>
2023-03-01 12:06:06 +0100

    Reimplementation of feature01 in different file

--- springAssignmentProj/src/main/java/com/ct/springAssignmentProj/greeting/GreetingRestController.java
+++ springAssignmentProj/src/main/java/com/ct/springAssignmentProj/greeting/GreetingRestController.java
@@ -1,7 +1,12 @@
 package com.ct.springAssignmentProj.greeting;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingRestController {
+    @GetMapping(value = "hello-rest")
+    public String feature01(){
+        return new String("Hello World");
+    }
 }
