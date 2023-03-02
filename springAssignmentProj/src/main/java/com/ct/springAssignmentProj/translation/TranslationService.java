10d830cadc7e35edec22d05315c857797c95a922
Mesaric <niko.mesar@gmail.com>
2023-03-02 12:08:35 +0100

    Updated exception handling on addTranslation. Created TranslationServiceTest with tests

--- springAssignmentProj/src/main/java/com/ct/springAssignmentProj/translation/TranslationService.java
+++ springAssignmentProj/src/main/java/com/ct/springAssignmentProj/translation/TranslationService.java
@@ -1,33 +1,40 @@
 package com.ct.springAssignmentProj.translation;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.ServletRequestBindingException;
 
 @Service
 @AllArgsConstructor
 public class TranslationService {
     private final static String NEW_TRANSLATION_ADDED_MSG = "Translation with text '%s' added.";
-    private final static String NEW_TRANSLATION_ADDED_UNSUCCESSFULLY_MSG = "Translation with text '%s' of language '%s' could not be added.";
+    private final static String INCORRECT_LANGUAGE_CODE_LENGTH = "Language code %s is of length %d (should be 2 characters long).";
+    private final static String INCORRECT_TRANSLATION_LENGTH = "Translation %s is of length %d (should be between 1 or 127 characters long).";
+    private final static String TRANSLATION_NOT_FOUND_MSG = "No translation of language %s was found";
     private final TranslationRepository translationRepository;
 
     public String getText(String language){
-        return translationRepository.findByLanguage(language.toUpperCase()).get().getText();
+        return translationRepository.findByLanguage(language.toUpperCase())
+                .orElseThrow(() -> new IllegalStateException(String.format(TRANSLATION_NOT_FOUND_MSG, language)))
+                .getText();
     }
 
-    public String addTranslation(Translation translation){
-        //todo: entry validation, UI warning for input
-        try {
-            translationRepository.save(
+    public String addTranslation(Translation translation) {
+        String sanitisedLanguageCode = translation.getLanguage().trim().toUpperCase();
+        if (sanitisedLanguageCode.length() != 2) {
+            throw new IllegalStateException(String.format(INCORRECT_LANGUAGE_CODE_LENGTH, sanitisedLanguageCode, sanitisedLanguageCode.length()));
+        }
+        if (translation.getText().length() == 0 || translation.getText().length() > 127) {
+            throw new IllegalStateException(String.format(INCORRECT_TRANSLATION_LENGTH, translation.getLanguage(), translation.getLanguage().length()));
+        }
+
+        translationRepository.save(
                 new Translation(
                         translation.getText(),
                         translation.getLanguage().toUpperCase()
                 ));
 
-            return String.format(NEW_TRANSLATION_ADDED_MSG, translation.getText());
-
-        }
-        catch (Exception exception) {
-            return String.format(NEW_TRANSLATION_ADDED_UNSUCCESSFULLY_MSG, translation.getText(), translation.getLanguage().toUpperCase());
-        }
+        return String.format(NEW_TRANSLATION_ADDED_MSG, translation.getText());
     }
 }
