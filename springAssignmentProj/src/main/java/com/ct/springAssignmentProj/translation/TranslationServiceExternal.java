127c8aa621c7b18e916e4b7fa447c13aaa3851f1
Mesaric <niko.mesar@gmail.com>
2023-03-03 08:23:20 +0100

    Added new okhttp3 dependency, updated README. Created empty TranslationServiceExternal

---
+++ springAssignmentProj/src/main/java/com/ct/springAssignmentProj/translation/TranslationServiceExternal.java
@@ -0,0 +1,10 @@
+package com.ct.springAssignmentProj.translation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TranslationServiceExternal {
+
+}
